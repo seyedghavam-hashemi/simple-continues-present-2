fa0b9092d65b6648138ffca26dd81696dbe314e3
GHAVAM <80568373+seyedghavam-hashemi@users.noreply.github.com>
2024-11-13 10:38:53 -0800

    Add files via upload

---
+++ js/dist/popup.dev.js
@@ -0,0 +1,110 @@
+"use strict";
+
+// When the user clicks on div, open the popup
+function myFunctionpop1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop5() {
+  var popup = document.getElementById("myPopup5");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop6() {
+  var popup = document.getElementById("myPopup6");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop7() {
+  var popup = document.getElementById("myPopup7");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop8() {
+  var popup = document.getElementById("myPopup8");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop9() {
+  var popup = document.getElementById("myPopup9");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop10() {
+  var popup = document.getElementById("myPopup10");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop11() {
+  var popup = document.getElementById("myPopup11");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
+
+function myFunctionpop12() {
+  var popup = document.getElementById("myPopup12");
+  popup.classList.toggle("show");
+  if (popup.classList.contains("show")) // Check if the popup is shown
+    setTimeout(function () {
+      return popup.classList.remove("show");
+    }, 2000); // If yes hide it after 2000 milliseconds
+}
---
+++ js/img-slider.js
@@ -0,0 +1,230 @@
+
+/*Question-1*/
+document.getElementById('btn1').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/2.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+
+function quDisNo1() {
+    document.getElementById("quDiv1").style.display = "none";
+  }
+function quDisBl2() {
+    document.getElementById("quDiv2").style.display = "block";
+  }
+function ansDisNo1() {
+    document.getElementById("ansDiv1").style.display = "none";
+  }
+function ansDisBl2() {
+    document.getElementById("ansDiv2").style.display = "block";
+  }
+
+
+/*Question-2*/
+document.getElementById('btn2').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/3.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+function quDisNo2() {
+    document.getElementById("quDiv2").style.display = "none";
+  }  
+function quDisBl3() {
+    document.getElementById("quDiv3").style.display = "block";
+  }
+  function ansDisNo2() {
+    document.getElementById("ansDiv2").style.display = "none";
+  }
+function ansDisBl3() {
+    document.getElementById("ansDiv3").style.display = "block";
+  }
+
+/*Question-3*/
+document.getElementById('btn3').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/4.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+function quDisNo3() {
+    document.getElementById("quDiv3").style.display = "none";
+  }
+function quDisBl4() {
+    document.getElementById("quDiv4").style.display = "block";
+  }
+  function ansDisNo3() {
+    document.getElementById("ansDiv3").style.display = "none";
+  }
+function ansDisBl4() {
+    document.getElementById("ansDiv4").style.display = "block";
+  }
+
+/*Question-4*/
+document.getElementById('btn4').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/5.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+
+function quDisNo4() {
+    document.getElementById("quDiv4").style.display = "none";
+  }
+function quDisBl5() {
+    document.getElementById("quDiv5").style.display = "block";
+  }
+  function ansDisNo4() {
+    document.getElementById("ansDiv4").style.display = "none";
+  }
+function ansDisBl5() {
+    document.getElementById("ansDiv5").style.display = "block";
+  }
+
+/*Question-5*/
+document.getElementById('btn5').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/6.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+function quDisNo5() {
+    document.getElementById("quDiv5").style.display = "none";
+  }
+function quDisBl6() {
+    document.getElementById("quDiv6").style.display = "block";
+  }  
+  function ansDisNo5() {
+    document.getElementById("ansDiv5").style.display = "none";
+  }
+function ansDisBl6() {
+    document.getElementById("ansDiv6").style.display = "block";
+  }
+/*Question-6*/
+  document.getElementById('btn6').addEventListener('click', function(){
+      document.body.style.backgroundImage= "url(image/bg/7.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+    });
+function quDisNo6() {
+      document.getElementById("quDiv6").style.display = "none";
+    }
+function quDisBl7() {
+      document.getElementById("quDiv7").style.display = "block";
+    }
+function ansDisNo6() {
+      document.getElementById("ansDiv6").style.display = "none";
+    }
+function ansDisBl7() {
+      document.getElementById("ansDiv7").style.display = "block";
+    }
+/*Question-7*/
+document.getElementById('btn7').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/8.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+
+function quDisNo7() {
+    document.getElementById("quDiv7").style.display = "none";
+  }
+
+function quDisBl8() {
+    document.getElementById("quDiv8").style.display = "block";
+  }
+  function ansDisNo7() {
+    document.getElementById("ansDiv7").style.display = "none";
+  }
+function ansDisBl8() {
+    document.getElementById("ansDiv8").style.display = "block";
+  }
+
+
+/*Question-8*/
+document.getElementById('btn8').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/9.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+function quDisNo8() {
+    document.getElementById("quDiv8").style.display = "none";
+  }
+function quDisBl9() {
+    document.getElementById("quDiv9").style.display = "block";
+  }  
+  function ansDisNo8() {
+    document.getElementById("ansDiv8").style.display = "none";
+  }
+function ansDisBl9() {
+    document.getElementById("ansDiv9").style.display = "block";
+  }
+
+/*Question-9*/
+document.getElementById('btn9').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/10.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+function quDisNo9() {
+    document.getElementById("quDiv9").style.display = "none";
+  }
+function quDisBl10() {
+    document.getElementById("quDiv10").style.display = "block";
+  }    
+  function ansDisNo9() {
+    document.getElementById("ansDiv9").style.display = "none";
+  }
+function ansDisBl10() {
+    document.getElementById("ansDiv10").style.display = "block";
+  }
+
+
+
+
+/*Question-10*/
+document.getElementById('btn10').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/11.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+function quDisNo10() {
+    document.getElementById("quDiv10").style.display = "none";
+  }  
+function quDisBl11() {
+    document.getElementById("quDiv11").style.display = "block";
+  }    
+function ansDisNo10() {
+    document.getElementById("ansDiv10").style.display = "none";
+  }  
+function ansDisBl11() {
+    document.getElementById("ansDiv11").style.display = "block";
+  }    
+
+/*Question-11*/
+document.getElementById('btn11').addEventListener('click', function(){
+    document.body.style.backgroundImage= "url(image/bg/12.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+  });
+
+function quDisNo11() {
+    document.getElementById("quDiv11").style.display = "none";
+  }  
+function quDisBl12() {
+    document.getElementById("quDiv12").style.display = "block";
+  }  
+
+  function ansDisNo11() {
+    document.getElementById("ansDiv11").style.display = "none";
+  }  
+function ansDisBl12() {
+    document.getElementById("ansDiv12").style.display = "block";
+  } 
+
+/*Question-12*/
+document.getElementById('btn12').addEventListener('click', function(){
+      document.body.style.backgroundImage= "url(image/bg/13.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+    });
+function quDisNo12() {
+      document.getElementById("quDiv12").style.display = "none";
+    }  
+function quDisBl13() {
+      document.getElementById("quDiv13").style.display = "block";
+    }      
+function ansDisNo12() {
+      document.getElementById("ansDiv12").style.display = "none";
+    }  
+  function ansDisBl13() {
+      document.getElementById("ansDiv13").style.display = "block";
+    }     
+ /*end-part-13*/
+document.getElementById('btn13').addEventListener('click', function(){
+  document.body.style.backgroundImage= "url(image/bg/1.jpg)";// I gave the address fom body itself. the address is relative to body not to JS file
+});
+function quDisNo13() {
+  document.getElementById("quDiv13").style.display = "none";
+}  
+function quDisBl1() {
+  document.getElementById("quDiv1").style.display = "block";
+}      
+function ansDisNo13() {
+  document.getElementById("ansDiv13").style.display = "none";
+}  
+function ansDisBl1() {
+  document.getElementById("ansDiv1").style.display = "block";
+}     
+   
